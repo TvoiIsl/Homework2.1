83a4596147f84116c6b451760bba131ff2f222bc
Islam <islam.bedretdinov@yandex.ru>
2022-12-16 19:46:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    int a = 1;
+    if(a>3){
+        System.out.println("а больше 3");
+    }else {
+        System.out.println("а меньше 3");
+    }
+}
+}
